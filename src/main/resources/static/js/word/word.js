a58c968c1565de14f9afa9fe25c527ee29692b47
Kotaro Chiba <kohtarocb@gmail.com>
2020-12-27 09:29:52 +0900

    wordクラスの作成

--- src/main/resources/static/js/word/word.js
+++ src/main/resources/static/js/word/word.js
@@ -7,5 +7,24 @@ class Word {
 		this.wordRome = wordRome;
 	}
 
+	get word() {
+		return this._word;
+	}
+
+	set word(value) {
+		this._word = value;
+	}
+
+	get wordRome() {
+		return this._wordRome;
+	}
+
+	set wordRome(value) {
+		this._wordRome = value;
+	}
+
+	getWord() {
+		return this.word + ":" + this.wordRome;
+	}
 
 }
