7598e77d20e68db7231559b738a89e758587aab5
Kotaro Chiba <kohtarocb@gmail.com>
2021-01-01 16:39:25 +0900

    DBの単語取得処理の追加

--- src/main/java/com/example/demo/web/TypingController.java
+++ src/main/java/com/example/demo/web/TypingController.java
@@ -1,10 +1,10 @@
 package com.example.demo.web;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
+import java.util.Random;
 
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -17,8 +17,11 @@ import com.example.demo.service.TypingService;
 @Controller
 public class TypingController {
 
+	final private int WORD_NUM = 4;
+
 	@Autowired
 	TypingService typingService;
+
 
 	/**
 	 * メニュー画面に遷移する。
@@ -38,23 +41,47 @@ public class TypingController {
 	 */
 	@PostMapping("/start")
 	public String start(Model model) {
-		// 単語idを全て取得
-		List<String> ids = typingService.selectId();
-		// todo JSONで試す
-		// 単語綴りのマップと単語ローマ字のマップをもつリストを格納するリストを生成
-		List<List<Map<String, String>>> words = new ArrayList<List<Map<String, String>>>();
-		for (String id: ids) {
-			Word word = typingService.selectById(id);
-			List<Map<String, String>> wordStrRm = new ArrayList<Map<String,String>>();
-			Map<String, String> wordString = new HashMap<String, String>();
-			wordString.put("wordString", word.getWord());
-			Map<String, String> wordRome = new HashMap<String, String>();
-			wordRome.put("wordRome", word.getWordRome());
-			wordStrRm.add(wordString);
-			wordStrRm.add(wordRome);
-			words.add(wordStrRm);
-		}
-		model.addAttribute("words", words);
+		//  単語idを取得
+		List<String> ids = getIds(WORD_NUM);
+		model.addAttribute("words", getWords(ids));
 		return "/start";
+	}
+
+	/**
+	 * 無作為に選ばれた単語idのリストを取得する。
+	 *
+	 * @param word_num 取得したい単語数
+	 * @return 単語idのリスト
+	 */
+	public List<String> getIds(int word_num) {
+		// 単語idを全て取得
+		List<String> idList = typingService.selectId();
+		List<String> ids = new ArrayList<String>();
+		for (int i = 0; i < word_num; i++) {
+			int rand = new Random().nextInt(idList.size());
+			ids.add(idList.get(rand));
+		}
+
+		return ids;
+	}
+
+	/**
+	 * 単語idリストをから単語を取得しJSON形式で返す。
+	 *
+	 * @param ids 単語idのリスト
+	 * @return JSON化された単語リスト
+	 */
+	public JSONObject getWords(List<String> ids) {
+		JSONObject words = new JSONObject();
+		int idx = 0;
+		for (String id: ids) {
+			idx++;
+			Word word = typingService.selectById(id);
+			JSONObject childNode = new JSONObject();
+			childNode.put("wordStr", word.getWordStr());
+			childNode.put("wordRome", word.getWordRome());
+			words.put("word" + idx, childNode);
+		}
+		return words;
 	}
 }
